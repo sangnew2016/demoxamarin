e44e6dec8c421d5b67b83ee5cffbb451c60a5e3d
sangnew2016 <sangnew2016@gmail.com>
2018-08-25 20:43:26 +0700

    set test xamarin - online

--- DemoApp/DemoApp/App.xaml.cs
+++ DemoApp/DemoApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +20,10 @@ namespace DemoApp
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("android=c0f12061-9130-4854-a93e-00f153dd9ded;" +
+                  "uwp=6a7b0d5a-1ef3-4634-bd61-b533c169ce1f;" +
+                  "ios=bff53c15-2afa-40b2-ad51-14996a6bb24b",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
